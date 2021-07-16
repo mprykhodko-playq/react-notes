1bbdf65848582cbec326e65e56b141784c169ee2
mikeprikhodko <mprykhodko@playq.net>
2021-07-16 14:09:10 +0300

    notes app

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">React приложение с хуками</h1>
+            <p className="lead">
+                Версия приложения <strong>1.01</strong>
+            </p>
+        </div>
+    </div>
+);
